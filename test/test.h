91575cc59d31b548a67a362f3bcc288ad0b5d172
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-09-27 21:13:59 +0200

    test: add stringify macro

--- test/test.h
+++ test/test.h
@@ -2,6 +2,14 @@
 
 #include <filesystem>
 
+#define __LK_TEST_STRINGIFY(x) #x
+
+/**
+ * @def LK_TEST_STRINGIFY
+ * @brief Expand token before stringifying it.
+ */
+#define LK_TEST_STRINGIFY(x) __LK_TEST_STRINGIFY(x)
+
 namespace platformer2d::test {
 
 	std::filesystem::path GetBinaryDirectory();
