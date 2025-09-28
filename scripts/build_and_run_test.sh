6c216073d40c05f30ce64d53156351578af7f9ea
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-09-28 12:17:36 +0200

    scripts: update test builder a bit
    Still very experimental

--- scripts/build_and_run_test.sh
+++ scripts/build_and_run_test.sh
@@ -2,9 +2,9 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit 1
 echo "script args: $@"
 
-cmake -S . -B build --fresh -DTEST_DRAW_TRIANGLE_WITH_SHADER=1
+cmake -S . -B build --fresh -DLK_TEST_DRAW_TRIANGLE_SHADER=1 -DLK_TEST_DRAW_TRIANGLE_SHADER_CONFIGURABLE=1
 cmake --build build -j 8
 
 echo ""
 echo "Running test"
-./build/Debug/platformer2d.exe
+./build/x64-Debug/test/draw_triangle_shader_configurable.exe
