d647ac5b64777582cbc204dd320827fc1441b24a
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-04 20:48:34 +0200

    add log macros

---
+++ src/core/log.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+#define LK_TRACE(...) spdlog::trace(__VA_ARGS__)
+#define LK_DEBUG(...) spdlog::debug(__VA_ARGS__)
+#define LK_INFO(...)  spdlog::info(__VA_ARGS__)
+#define LK_WARN(...)  spdlog::warn(__VA_ARGS__)
+#define LK_ERROR(...)  spdlog::error(__VA_ARGS__)
