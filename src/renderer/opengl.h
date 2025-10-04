1966ad64139ff879040a6fe1f49885002b78b192
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-04 20:46:18 +0200

    add helper functions to OpenGL namespace

--- src/renderer/opengl.h
+++ src/renderer/opengl.h
@@ -3,17 +3,18 @@
 #include <glad/glad.h>
 
 #include "core/assert.h"
+#include "renderer/vertexbufferlayout.h"
 
-namespace OpenGL_Internal 
+namespace platformer2d::OpenGL_Internal 
 {
-	static inline void CheckForErrors()
+	FORCEINLINE static void CheckForErrors()
 	{
 		while (glGetError() != GL_NO_ERROR)
 		{
 		}
 	}
 
-	static inline bool VerifyFunctionResult(const char* InFunction, const char* InFile, int InLine)
+	FORCEINLINE static bool VerifyFunctionResult(const char* InFunction, const char* InFile, int InLine)
 	{
 		while (GLenum Error = glGetError())
 		{
@@ -29,3 +30,37 @@ namespace OpenGL_Internal
 	OpenGL_Internal::CheckForErrors();  \
 	OpenGLFunction;                      \
 	LK_VERIFY(OpenGL_Internal::VerifyFunctionResult(#OpenGLFunction, __FILE__, __LINE__))
+
+namespace platformer2d::OpenGL {
+
+	namespace VertexBuffer {
+		static inline void Initialize(GLuint* VBO, void* Data, const std::size_t DataSize)
+		{
+			LK_ASSERT(VBO && Data && (DataSize > 0));
+			glGenBuffers(1, VBO);
+			glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+			glBufferData(GL_ARRAY_BUFFER, DataSize, Data, GL_STATIC_DRAW);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
+		}
+	}
+
+	FORCEINLINE static constexpr GLenum ShaderDataTypeToOpenGLBaseType(const EShaderDataType Type)
+	{
+		switch (Type)
+		{
+			case EShaderDataType::Float:  return GL_FLOAT;
+			case EShaderDataType::Float2: return GL_FLOAT;
+			case EShaderDataType::Float3: return GL_FLOAT;
+			case EShaderDataType::Float4: return GL_FLOAT;
+			case EShaderDataType::Mat3:	  return GL_FLOAT;
+			case EShaderDataType::Mat4:	  return GL_FLOAT;
+			case EShaderDataType::Int:
+			case EShaderDataType::Int2:
+			case EShaderDataType::Int3:
+			case EShaderDataType::Int4:	  return GL_INT;
+			case EShaderDataType::Bool:	  return GL_BOOL;
+		}
+		return GL_INVALID_ENUM;
+	}
+
+}
