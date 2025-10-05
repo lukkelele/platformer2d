fc8439d0844ecab769f6d4fbcbf3d143cae92361
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-05 17:34:20 +0200

    renderer: setup texture filter and wrapping

--- src/renderer/texture.cpp
+++ src/renderer/texture.cpp
@@ -1,18 +1,16 @@
 #include "texture.h"
 
-#include <type_traits>
-
 #include <stb/stb_image.h>
-#include <spdlog/spdlog.h>
 
-#include "core/assert.h"
+#include "core/log.h"
 #include "renderer/opengl.h"
 
 namespace platformer2d {
 
+	static_assert(std::is_same_v<LRendererID, GLuint>, "RendererID type mismatch");
+
 	CTexture::CTexture()
 	{
-		static_assert(std::is_same_v<decltype(RendererID), GLuint>, "RendererID type mismatch");
 	}
 
 	CTexture::CTexture(const uint16_t InWidth, const uint16_t InHeight, void* InData)
@@ -20,17 +18,20 @@ namespace platformer2d {
 		, Height(InHeight)
 	{
 		LK_ASSERT((InWidth > 0) && (InHeight > 0));
-
 		LK_OpenGL_Verify(glGenTextures(1, &RendererID));
 		LK_OpenGL_Verify(glBindTexture(GL_TEXTURE_2D, RendererID));
-		spdlog::info("RendererID={}", RendererID);
 
+		/**
+		 * @todo Pass texture wrap/filter args from constructor.
+		 * Also pass other metadata to know if stbi_load/stbi_loadf was used
+		 * to correctly map GL_FLOAT/GL_UNSIGNED_BYTE.
+		 */
 		static const GLenum ImageFormat = GL_RGBA;
 		static const GLenum InternalImageFormat = GL_RGBA32F;
 
 		if (InData)
 		{
-			spdlog::info("glTexImage2D");
+			LK_DEBUG("glTexImage2D");
 			LK_OpenGL_Verify(glTexImage2D(
 				GL_TEXTURE_2D, 
 				0, 
@@ -39,13 +40,27 @@ namespace platformer2d {
 				Height,
 				0,
 				ImageFormat, 
-				GL_UNSIGNED_BYTE, 
+				GL_FLOAT, 
 				(const void*)InData)
 			);
 
 			spdlog::warn("Release texture data");
 			stbi_image_free(InData);
 		}
+
+		/* Wrap. */
+		/* @fixme */
+		LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+		LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+		//LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT));
+		//LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT));
+
+		/* Filter. */
+		/* @fixme */
+		LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST));
+		LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+		//LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+		//LK_OpenGL_Verify(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
 	}
 
 }
--- src/renderer/texture.h
+++ src/renderer/texture.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <stdint.h>
+#include "core/assert.h"
+#include "core/core.h"
+#include "opengl.h"
 
 namespace platformer2d {
 
@@ -11,8 +13,22 @@ namespace platformer2d {
 		CTexture(uint16_t InWidth, uint16_t InHeight, void* InData = nullptr);
 		~CTexture() = default;
 
+		FORCEINLINE void Bind(const uint32_t Slot = 0) const
+		{
+			LK_OpenGL_Verify(glActiveTexture(GL_TEXTURE0 + Slot));
+			LK_OpenGL_Verify(glBindTextureUnit(Slot, RendererID));
+		}
+
+		FORCEINLINE void Unbind(const uint32_t Slot = 0) const
+		{
+			/* LK_OpenGL_Verify(glActiveTexture(GL_TEXTURE0 + Slot)); */ /* @todo Needed or not? */
+			LK_OpenGL_Verify(glBindTextureUnit(Slot, 0));
+		}
+
+		LRendererID GetRendererID() const { return RendererID; }
+
 	private:
-		uint32_t RendererID{};
+		LRendererID RendererID{};
 		uint16_t Width{};
 		uint16_t Height{};
 	};
