fa3afb19f13c093ce31d06f3a4a8e3e5fa3eb98c
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-13 18:44:49 +0200

    core: add ansi color defines

---
+++ src/core/ansi_colors.h
@@ -0,0 +1,43 @@
+#pragma once
+
+/**
+ * ANSI colors. 
+ */
+
+#define LK_ANSI_COLOR_RESET              "\033[0m"
+
+/* Foreground Colors */
+#define LK_ANSI_COLOR_BLACK              "\033[30m"
+#define LK_ANSI_COLOR_RED                "\033[31m"
+#define LK_ANSI_COLOR_GREEN              "\033[32m"
+#define LK_ANSI_COLOR_YELLOW             "\033[33m"
+#define LK_ANSI_COLOR_BLUE               "\033[34m"
+#define LK_ANSI_COLOR_MAGENTA            "\033[35m"
+#define LK_ANSI_COLOR_CYAN               "\033[36m"
+#define LK_ANSI_COLOR_WHITE              "\033[37m"
+#define LK_ANSI_COLOR_BRIGHT_BLACK       "\033[90m"
+#define LK_ANSI_COLOR_BRIGHT_RED         "\033[91m"
+#define LK_ANSI_COLOR_BRIGHT_GREEN       "\033[92m"
+#define LK_ANSI_COLOR_BRIGHT_YELLOW      "\033[93m"
+#define LK_ANSI_COLOR_BRIGHT_BLUE        "\033[94m"
+#define LK_ANSI_COLOR_BRIGHT_MAGENTA     "\033[95m"
+#define LK_ANSI_COLOR_BRIGHT_CYAN        "\033[96m"
+#define LK_ANSI_COLOR_BRIGHT_WHITE       "\033[97m"
+
+/* Background Colors */
+#define LK_ANSI_COLOR_BG_BLACK           "\033[40m"
+#define LK_ANSI_COLOR_BG_RED             "\033[41m"
+#define LK_ANSI_COLOR_BG_GREEN           "\033[42m"
+#define LK_ANSI_COLOR_BG_YELLOW          "\033[43m"
+#define LK_ANSI_COLOR_BG_BLUE            "\033[44m"
+#define LK_ANSI_COLOR_BG_MAGENTA         "\033[45m"
+#define LK_ANSI_COLOR_BG_CYAN            "\033[46m"
+#define LK_ANSI_COLOR_BG_WHITE           "\033[47m"
+#define LK_ANSI_COLOR_BG_BRIGHT_BLACK    "\033[100m"
+#define LK_ANSI_COLOR_BG_BRIGHT_RED      "\033[101m"
+#define LK_ANSI_COLOR_BG_BRIGHT_GREEN    "\033[102m"
+#define LK_ANSI_COLOR_BG_BRIGHT_YELLOW   "\033[103m"
+#define LK_ANSI_COLOR_BG_BRIGHT_BLUE     "\033[104m"
+#define LK_ANSI_COLOR_BG_BRIGHT_MAGENTA  "\033[105m"
+#define LK_ANSI_COLOR_BG_BRIGHT_CYAN     "\033[106m"
+#define LK_ANSI_COLOR_BG_BRIGHT_WHITE    "\033[107m"
