7f85b7998ef5a4207b00416c2c4cc76395c35c1e
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-15 21:42:03 +0200

    scripts: add extensions to build_glad.sh

--- scripts/build_glad.sh
+++ scripts/build_glad.sh
@@ -1,3 +1,30 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE[0]}")"/.. || exit 1
+
 python -m pip install glad --break-system-packages
-# todo: check current path to know if to traverse ../ or not
-python -m glad --profile=core --api=gl=4.6 --generator=c --out-path=modules/glad
+
+set -- \
+  GL_ARB_bindless_texture \
+  GL_ARB_texture_storage \
+  GL_ARB_texture_buffer_object \
+  GL_ARB_depth_texture \
+  GL_ARB_draw_instanced \
+  GL_ARB_draw_buffers \
+  GL_ARB_texture_cube_map \
+  GL_ARB_texture_cube_map_array \
+  GL_ARB_texture_multisample \
+  GL_ARB_instanced_arrays \
+  GL_ARB_sample_shading \
+  GL_ARB_debug_output \
+  GL_ARB_timer_query \
+  GL_ARB_direct_state_access
+
+extensions=$(printf '%s,' "$@")
+extensions=${extensions%,} # Drop trailing comma.
+
+python -m glad \
+  --profile=core \
+  --api=gl=4.6 \
+  --generator=c \
+  --out-path=modules/glad \
+  --extensions="${extensions}"
