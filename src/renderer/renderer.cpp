ab6fdc2190814386b676a85fee43527926933704
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-15 21:49:16 +0200

    renderer: add quad batch rendering

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -25,39 +25,37 @@ namespace platformer2d {
 
 	struct FRendererData
 	{
-		struct FQuad 
+		struct FLine
 		{
 			GLuint VAO = 0;
 			GLuint VBO = 0;
-			GLuint EBO = 0;
-		} Quad;
+		} Line;
+		std::unique_ptr<CShader> LineShader = nullptr;
 
 		std::shared_ptr<CTexture> WhiteTexture = nullptr;
 	};
 
 	namespace 
 	{
-		FRendererData Data{};
+		constexpr uint32_t MaxQuads = 10000;
+		constexpr uint32_t MaxLines = 1000;
+		constexpr uint32_t MaxVertices = MaxQuads * 4;
+		constexpr uint32_t MaxIndices = MaxQuads * 6;
+		constexpr uint32_t MaxLineVertices = MaxLines * 2;
+		constexpr uint32_t MaxLineIndices = MaxLines * 6;
 
-		std::unordered_map<std::string, std::shared_ptr<CShader>> ShaderLibrary{};
-		const std::string SHADER_QUAD = "Quad";
+		FRendererData Data{};
 
 		std::array<CRenderCommandQueue*, 2> CommandQueue;
 		std::atomic<uint32_t> CommandQueueSubmissionIndex = 0;
 
 		OpenGL::FBackendInfo BackendInfo{};
 
-		constexpr float QuadVertices[] = {
-		  /*  Position    Texture Coordinates */
-			-1.0f, -1.0f,    0.0f, 0.0f,
-			 1.0f, -1.0f,    1.0f, 0.0f,
-			 1.0f,  1.0f,    1.0f, 1.0f,
-			-1.0f,  1.0f,    0.0f, 1.0f
-		};
-
-		constexpr uint32_t QuadIndices[] = { 
-			0, 1, 2,  
-			2, 3, 0 
+		constexpr glm::vec2 TextureCoords[] = {
+			{ 0.0f, 0.0f }, /*  Bottom Left.  */
+			{ 0.0f, 1.0f }, /*  Top Left.     */
+			{ 1.0f, 1.0f }, /*  Top Right.    */
+			{ 1.0f, 0.0f }  /*  Bottom Right. */
 		};
 	}
 
@@ -71,6 +69,12 @@ namespace platformer2d {
 
 		OpenGL::LoadInfo(BackendInfo);
 		LK_INFO("OpenGL {}.{}", BackendInfo.Version.Major, BackendInfo.Version.Minor);
+#ifdef LK_LOG_OPENGL_EXTENSIONS
+		for (const std::string& Extension : BackendInfo.Extensions)
+		{
+			LK_INFO("Extension: {}", Extension);
+		}
+#endif
 
 		LK_DEBUG_TAG("Renderer", "Creating {} render command queues", CommandQueue.size());
 		for (int Idx = 0; Idx < CommandQueue.size(); Idx++)
@@ -78,17 +82,131 @@ namespace platformer2d {
 			CommandQueue[Idx] = new CRenderCommandQueue();
 		}
 
-		const FVertexBufferLayout QuadLayout = {
-			{ "pos",      EShaderDataType::Float2, },
-			{ "texcoord", EShaderDataType::Float2, },
-		};
+		/* Quad */
+		{
+			QuadVertexPositions[0] = { -0.50f, -0.50f, 0.0f, 1.0f };
+			QuadVertexPositions[1] = { -0.50f,  0.50f, 0.0f, 1.0f };
+			QuadVertexPositions[2] = {  0.50f,  0.50f, 0.0f, 1.0f };
+			QuadVertexPositions[3] = {  0.50f, -0.50f, 0.0f, 1.0f };
 
-		LK_TRACE_TAG("Renderer", "Creating quad VAO, VBO and EBO");
-		Data.Quad.VAO = OpenGL::VertexArray::Create();
-		Data.Quad.VBO = OpenGL::VertexBuffer::Create(QuadVertices, QuadLayout);
-		Data.Quad.EBO = OpenGL::ElementBuffer::Create(QuadIndices);
-		LK_DEBUG_TAG("Renderer", "Quad VAO={} VBO={} EBO={}", Data.Quad.VAO, Data.Quad.VBO, Data.Quad.EBO);
-		LK_VERIFY((Data.Quad.VAO != 0) && (Data.Quad.VBO != 0) && (Data.Quad.EBO != 0));
+			const FVertexBufferLayout QuadLayout = {
+				{ "pos",        EShaderDataType::Float4, },
+				{ "color",      EShaderDataType::Float4, },
+				{ "texcoord",   EShaderDataType::Float2, },
+				{ "texindex",   EShaderDataType::Float,  },
+				{ "tilefactor", EShaderDataType::Float,  },
+			};
+
+			/* Quad VAO. */
+			LK_OpenGL_Verify(glGenVertexArrays(1, &QuadVAO));
+			LK_OpenGL_Verify(glBindVertexArray(QuadVAO));
+
+			/* Quad VBO. */
+			LK_OpenGL_Verify(glGenBuffers(1, &QuadVBO));
+			LK_OpenGL_Verify(glBindBuffer(GL_ARRAY_BUFFER, QuadVBO));
+			LK_OpenGL_Verify(glBufferData(GL_ARRAY_BUFFER, MaxVertices * sizeof(FQuadVertex), nullptr, GL_STATIC_DRAW));
+
+			glEnableVertexAttribArray(0);
+			glVertexAttribPointer(
+				0,
+				3,
+				GL_FLOAT,
+				GL_FALSE,
+				sizeof(FQuadVertex),
+				(const void*)(offsetof(FQuadVertex, Position)) // (const void*)0
+			);
+			static_assert(0 * sizeof(float) == offsetof(FQuadVertex, Position));
+
+			glEnableVertexAttribArray(1);
+			glVertexAttribPointer(
+				1,
+				4,
+				GL_FLOAT,
+				GL_FALSE,
+				sizeof(FQuadVertex),
+				(const void*)(offsetof(FQuadVertex, Color)) // (const void*)(3 * sizeof(float))
+			);
+			static_assert(3 * sizeof(float) == offsetof(FQuadVertex, Color));
+
+			glEnableVertexAttribArray(2);
+			glVertexAttribPointer(
+				2,
+				2,
+				GL_FLOAT,
+				GL_FALSE,
+				sizeof(FQuadVertex),
+				(const void*)(offsetof(FQuadVertex, TexCoord))
+			);
+			static_assert(7 * sizeof(float) == offsetof(FQuadVertex, TexCoord));
+
+			glEnableVertexAttribArray(3);
+			glVertexAttribPointer(
+				3,
+				1,
+				GL_FLOAT,
+				GL_FALSE,
+				sizeof(FQuadVertex),
+				(const void*)(offsetof(FQuadVertex, TexIndex)) // (const void*)(9 * sizeof(float))
+			);
+			static_assert(9 * sizeof(float) == offsetof(FQuadVertex, TexIndex));
+
+			glEnableVertexAttribArray(4);
+			glVertexAttribPointer(
+				4,
+				1,
+				GL_FLOAT,
+				GL_FALSE,
+				sizeof(FQuadVertex),
+				(const void*)(offsetof(FQuadVertex, TileFactor)) // (const void*)(10 * sizeof(float))
+			);
+			static_assert(10 * sizeof(float) == offsetof(FQuadVertex, TileFactor));
+
+			QuadShader = std::make_unique<CShader>(SHADERS_DIR "/quad.shader");
+			QuadShader->Set("u_proj", glm::mat4(1.0f));
+			QuadShader->Set("u_texarray", 0); /* @fixme */
+
+			QuadVertexBufferBase = new FQuadVertex[MaxVertices];
+			uint32_t* QuadIndices = new uint32_t[MaxIndices];
+			uint32_t Offset = 0;
+			for (uint32_t Idx = 0; Idx < MaxIndices; Idx += 6)
+			{
+				/* First triangle, 0->1->2 */
+				QuadIndices[Idx + 0] = Offset + 0;
+				QuadIndices[Idx + 1] = Offset + 1;
+				QuadIndices[Idx + 2] = Offset + 2;
+
+				/* Second triangle, 2->3->0 */
+				QuadIndices[Idx + 3] = Offset + 2;
+				QuadIndices[Idx + 4] = Offset + 3;
+				QuadIndices[Idx + 5] = Offset + 0;
+
+				Offset += 4;
+			}
+
+			/* Quad EBO. */
+			LK_OpenGL_Verify(glCreateBuffers(1, &QuadEBO));
+			LK_OpenGL_Verify(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, QuadEBO));
+			LK_OpenGL_Verify(glNamedBufferData(QuadEBO, MaxIndices * sizeof(uint32_t), QuadIndices, GL_STATIC_DRAW));
+
+			QuadVertexBufferPtr = QuadVertexBufferBase;
+
+			delete[] QuadIndices;
+		}
+
+		/* Line */
+		{
+			glGenVertexArrays(1, &Data.Line.VAO);
+			glGenBuffers(1, &Data.Line.VBO);
+
+			glBindVertexArray(Data.Line.VAO);
+			glBindBuffer(GL_ARRAY_BUFFER, Data.Line.VBO);
+			/* Two vectors: 2 * 2 * sizeof(float) */
+			glBufferData(GL_ARRAY_BUFFER, (2 * 2 * sizeof(float)), nullptr, GL_DYNAMIC_DRAW);
+			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, (2 * sizeof(float)), nullptr);
+			glEnableVertexAttribArray(0);
+
+			Data.LineShader = std::make_unique<CShader>(SHADERS_DIR "/line.shader");
+		}
 
 		const char* WhiteTexturePath = TEXTURES_DIR "/white.png";
 		LK_TRACE_TAG("Renderer", "Load white texture");
@@ -101,24 +219,6 @@ namespace platformer2d {
 			.SamplerFilter = ETextureFilter::Nearest,
 		};
 		Data.WhiteTexture = std::make_shared<CTexture>(WhiteTextureSpec);
-
-		LK_INFO_TAG("Renderer", "Loading shaders from: {}", SHADERS_DIR);
-
-		/* Shader: Quad */
-		{
-			auto [Iter, Inserted] = ShaderLibrary.try_emplace(SHADER_QUAD, std::make_shared<CShader>(SHADERS_DIR "/player.shader"));
-			LK_VERIFY(Inserted == true);
-			std::shared_ptr<CShader>& Shader = Iter->second;
-			Shader->Set("u_texture", 0);
-		}
-
-#ifdef LK_BUILD_DEBUG
-		int Idx = 0;
-		for (const auto& [ShaderName, Shader] : ShaderLibrary)
-		{
-			LK_INFO_TAG("Renderer", "Shader {}: {} ({})", ++Idx, Shader->GetFilepath().filename(), ShaderName);
-		}
-#endif
 
 		ImGuiLayer = std::make_unique<CImGuiLayer>(CWindow::Get()->GetGlfwWindow());
 	}
@@ -136,23 +236,75 @@ namespace platformer2d {
 		ImGuiLayer->EndFrame();
 	}
 
+	void CRenderer::StartBatch()
+	{
+		QuadIndexCount = 0;
+		QuadVertexBufferPtr = QuadVertexBufferBase;
+	}
+
+	void CRenderer::NextBatch()
+	{
+		Flush();
+		StartBatch();
+	}
+
+	void CRenderer::Flush()
+	{
+		if (QuadIndexCount)
+		{
+			const uint32_t DataSize = static_cast<uint32_t>((uint8_t*)QuadVertexBufferPtr - (uint8_t*)QuadVertexBufferBase);
+			LK_OpenGL_Verify(glBindBuffer(GL_ARRAY_BUFFER, QuadVBO));
+			LK_OpenGL_Verify(glBufferSubData(GL_ARRAY_BUFFER, 0, DataSize, QuadVertexBufferBase));
+
+			QuadShader->Bind();
+			Data.WhiteTexture->Bind();
+			LK_OpenGL_Verify(glBindVertexArray(QuadVAO));
+			LK_OpenGL_Verify(glDrawElements(GL_TRIANGLES, QuadIndexCount, GL_UNSIGNED_INT, nullptr));
+			Data.WhiteTexture->Unbind();
+			QuadShader->Unbind();
+		}
+	}
+
 	void CRenderer::DrawQuad(const glm::vec2& Pos, const glm::vec2& Size, 
 							 const glm::vec4& Color, const float RotationDeg)
 	{
-		glm::mat4 Transform = glm::mat4(1.0f);
-		Transform = glm::translate(Transform, glm::vec3(Pos, 0.0f));
+		if (QuadIndexCount >= MaxIndices)
+		{
+			NextBatch();
+		}
 
-		Transform = glm::translate(Transform, glm::vec3(Size.x * 0.50f, Size.y * 0.50f, 0.0f));
-		Transform = glm::rotate(Transform, glm::radians(RotationDeg), glm::vec3(0.0f, 0.0f, 1.0f)); 
-		Transform = glm::translate(Transform, glm::vec3(Size.x * -0.50f, Size.y * -0.50f, 0.0f));
+		static constexpr float TextureIndex = 0.0f;
+		static constexpr float TileFactor = 1.0f;
 
-		Transform = glm::scale(Transform, glm::vec3(Size, 1.0f));
+		const glm::mat4 Transform = glm::translate(glm::mat4(1.0f), { Pos.x, Pos.y, 0.0f })
+            * glm::scale(glm::mat4(1.0f), { Size.x, Size.y, 1.0f });
 
-		ShaderLibrary[SHADER_QUAD]->Set("u_transform", Transform);
-		ShaderLibrary[SHADER_QUAD]->Set("u_color", Color);
+		for (std::size_t i = 0; i < 4; i++)
+		{
+			QuadVertexBufferPtr->Position = Transform * QuadVertexPositions[i];
+			QuadVertexBufferPtr->Color = Color;
+			QuadVertexBufferPtr->TexCoord = TextureCoords[i];
+			QuadVertexBufferPtr->TexIndex = TextureIndex;
+			QuadVertexBufferPtr->TileFactor = TileFactor;
+			QuadVertexBufferPtr++;
+		}
 
-		LK_OpenGL_Verify(glBindVertexArray(Data.Quad.VAO));
-		LK_OpenGL_Verify(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0));
+		QuadIndexCount += 6;
+	}
+
+	void CRenderer::DrawLine(const glm::vec2& P1, const glm::vec2& P2,
+							 const uint16_t LineWidth, const glm::vec4& Color)
+	{
+		Data.LineShader->Set("u_transform", glm::mat4(1.0f));
+		Data.LineShader->Set("u_color", { 1.0f, 0.50f, 1.0f, 1.0f });
+
+		const float Vertices[2][2] = { { P1.x, P1.y }, { P2.x, P2.y } };
+		LK_OpenGL_Verify(glBindBuffer(GL_ARRAY_BUFFER, Data.Line.VBO));
+		LK_OpenGL_Verify(glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(Vertices), Vertices));
+
+		LK_OpenGL_Verify(glBindVertexArray(Data.Line.VAO));
+		LK_OpenGL_Verify(glLineWidth(LineWidth));
+		LK_OpenGL_Verify(glDrawArrays(GL_LINES, 0, 2));
 	}
 
 }
--- src/renderer/renderer.h
+++ src/renderer/renderer.h
@@ -10,6 +10,21 @@
 
 namespace platformer2d {
 
+	struct FQuadVertex
+	{
+		glm::vec3 Position{};
+		glm::vec4 Color{};
+		glm::vec2 TexCoord{};
+		float TexIndex{};
+		float TileFactor{};
+	};
+
+	struct FLineVertex
+	{
+		glm::vec3 Position{};
+		glm::vec4 Color{};
+	};
+
 	class CRenderer
 	{
 	public:
@@ -18,22 +33,38 @@ namespace platformer2d {
 		CRenderer(const CRenderer&) = delete;
 		CRenderer(CRenderer&&) = delete;
 
-		CRenderer& operator=(const CRenderer&) = delete;
-		CRenderer& operator=(CRenderer&&) = delete;
-
 		static void Initialize();
 		static void Destroy();
 		static void BeginFrame();
 		static void EndFrame();
 
+		static void StartBatch();
+		static void NextBatch();
+		static void Flush();
+
 		static void DrawQuad(const glm::vec2& Pos, const glm::vec2& Size, 
 							 const glm::vec4& Color = {1.0f, 1.0f, 1.0f, 1.0f}, float RotationDeg = 0.0f);
+		static void DrawLine(const glm::vec2& P1, const glm::vec2& P2, uint16_t LineWidth = 8,
+							 const glm::vec4& Color = { 1.0f, 1.0f, 1.0f, 1.0f });
 
 		static void SetClearColor(const glm::vec4& InClearColor) { ClearColor = InClearColor; }
 
 	private:
+		CRenderer& operator=(const CRenderer&) = delete;
+		CRenderer& operator=(CRenderer&&) = delete;
+
+	private:
 		inline static glm::vec4 ClearColor{ 0.20f, 0.20f, 0.20f, 1.0f };
 		inline static std::unique_ptr<CImGuiLayer> ImGuiLayer = nullptr;
+
+		inline static GLuint QuadVAO = 0;
+		inline static GLuint QuadVBO = 0;
+		inline static GLuint QuadEBO = 0;
+		inline static uint32_t QuadIndexCount = 0;
+		inline static glm::vec4 QuadVertexPositions[4] = {};
+		inline static FQuadVertex* QuadVertexBufferBase = nullptr;
+		inline static FQuadVertex* QuadVertexBufferPtr = nullptr;
+		inline static std::unique_ptr<CShader> QuadShader = nullptr;
 	};
 
 }
