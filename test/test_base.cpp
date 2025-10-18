fbe9c8cba3753e854e2347200e494ecbeaf264b7
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-18 22:08:23 +0200

    test: use static func for SetDarkTheme

--- test/test_base.cpp
+++ test/test_base.cpp
@@ -18,6 +18,7 @@
 
 #include "core/log.h"
 #include "renderer/opengl.h"
+#include "renderer/imguilayer.h"
 
 #ifndef LK_TEST_SUITE
 #error "LK_TEST_SUITE not defined"
@@ -88,8 +89,6 @@ namespace platformer2d::test {
 #endif
 	}
 
-	void SetDarkTheme();
-
 	CTestBase::CTestBase(int Argc, char* Argv[])
 		: Args(Argc, Argv)
 		, BinaryDir(GetBinaryDir())
@@ -123,7 +122,7 @@ namespace platformer2d::test {
 		ImGui_ImplGlfw_InitForOpenGL(GlfwWindow, true);
 		ImGui_ImplOpenGL3_Init("#version 450");
 
-		SetDarkTheme();
+		CImGuiLayer::SetDarkTheme();
 
 		/** @todo Use generated header */
 		const char* SourceSansPro_Semibold = FONTS_DIR "/SourceCodePro/SourceSansPro-Semibold.ttf";
@@ -210,55 +209,6 @@ namespace platformer2d::test {
 		}
 
 		return bSetBlendFunc;
-	}
-
-	void SetDarkTheme()
-    {
-		ImGui::StyleColorsDark();
-		auto& Colors = ImGui::GetStyle().Colors;
-
-		/* Window background. */
-		Colors[ImGuiCol_WindowBg]		= ImVec4(0.10f, 0.105f, 0.110f, 1.0f);
-
-		/* Headers. */
-		Colors[ImGuiCol_Header]			= ImVec4(0.20f, 0.205f,  0.210f, 1.0f);
-		Colors[ImGuiCol_HeaderHovered]	= ImVec4(0.30f, 0.305f,  0.310f, 1.0f);
-		Colors[ImGuiCol_HeaderActive]	= ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-
-		/* Buttons. */
-		Colors[ImGuiCol_Button]			= ImVec4(0.20f, 0.205f, 0.210f, 1.0f);
-		Colors[ImGuiCol_ButtonHovered]	= ImVec4(0.30f, 0.305f, 0.310f, 1.0f);
-		Colors[ImGuiCol_ButtonActive]	= ImVec4(0.15f, 0.150f, 0.151f, 1.0f);
-
-		/* Frame background. */
-		Colors[ImGuiCol_FrameBg]		= ImVec4(0.20f, 0.2050f, 0.210f, 1.0f);
-		Colors[ImGuiCol_FrameBgHovered]	= ImVec4(0.30f, 0.3050f, 0.310f, 1.0f);
-		Colors[ImGuiCol_FrameBgActive]	= ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-
-		/* Tabs. */
-		Colors[ImGuiCol_Tab]				= ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-		Colors[ImGuiCol_TabHovered]			= ImVec4(0.38f, 0.3805f, 0.381f, 1.0f);
-		Colors[ImGuiCol_TabActive]			= ImVec4(0.28f, 0.2805f, 0.281f, 1.0f);
-		Colors[ImGuiCol_TabUnfocused]		= ImVec4(0.15f, 0.1505f, 0.151f, 1.0f);
-		Colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.20f, 0.2050f, 0.210f, 1.0f);
-
-		/* Title. */
-		Colors[ImGuiCol_TitleBg]		  = ImVec4(0.150f, 0.1505f, 0.151f, 1.0f);
-		Colors[ImGuiCol_TitleBgActive]	  = ImVec4(0.150f, 0.1505f, 0.151f, 1.0f);
-		Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.150f, 0.1505f, 0.151f, 1.0f);
-
-		/* Scrollbar. */
-		Colors[ImGuiCol_ScrollbarBg]		  = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
-		Colors[ImGuiCol_ScrollbarGrab]		  = ImVec4(0.31f, 0.31f, 0.31f, 1.0f);
-		Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.0f);
-		Colors[ImGuiCol_ScrollbarGrabActive]  = ImVec4(0.51f, 0.51f, 0.51f, 1.0f);
-
-		/* Checkmark. */
-		Colors[ImGuiCol_CheckMark] = ImVec4(0.94f, 0.94f, 0.94f, 1.0f);
-
-		/* Slider. */
-		Colors[ImGuiCol_SliderGrab]		  = ImVec4(0.51f, 0.51f, 0.51f, 0.7f);
-		Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.66f, 0.66f, 0.66f, 1.0f);
 	}
 
 }
