833188e053b5e117d07758a0d80af97a3c362f2f
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-19 22:06:04 +0200

    renderer: add texturearray
    Not implemented.
    
    For sampler2DArray

---
+++ src/renderer/texturearray.cpp
@@ -0,0 +1,21 @@
+#include "texturearray.h"
+
+namespace platformer2d {
+
+	CTextureArray::CTextureArray()
+	{
+	}
+
+	CTextureArray::CTextureArray(const FTextureSpecification& Specification)
+	{
+	}
+
+	CTextureArray::CTextureArray(uint32_t InWidth, uint32_t InHeight, void* InData)
+	{
+	}
+
+	CTextureArray::~CTextureArray()
+	{
+	}
+
+}
---
+++ src/renderer/texturearray.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <filesystem>
+
+#include "core/core.h"
+#include "core/assert.h"
+#include "core/buffer.h"
+#include "opengl.h"
+#include "texture_enums.h"
+
+namespace platformer2d {
+
+	class CTextureArray
+	{
+	public:
+		CTextureArray();
+		CTextureArray(const FTextureSpecification& Specification);
+		CTextureArray(uint32_t InWidth, uint32_t InHeight, void* InData = nullptr);
+		~CTextureArray();
+
+		void Bind(uint32_t Slot = 0) const;
+		void Unbind(uint32_t Slot = 0) const;
+
+		void Invalidate();
+
+		LRendererID GetRendererID() const { return RendererID; }
+		uint32_t GetWidth() const { return Width; }
+		uint32_t GetHeight() const { return Height; }
+		uint32_t GetChannels() const { return Channels; }
+		const std::filesystem::path& GetFilePath() const { return Path; }
+
+		std::size_t GetIndex() const { return Index; }
+		void SetIndex(std::size_t InIndex);
+
+	private:
+		LRendererID RendererID{};
+		FBuffer ImageData;
+		std::size_t Index;
+
+		uint32_t Width = 1;
+		uint32_t Height = 1;
+		uint8_t Channels;
+		std::filesystem::path Path{};
+
+		GLenum Format{};
+		GLenum InternalFormat{};
+		GLenum DataType{};
+		
+		static_assert(std::is_same_v<LRendererID, GLuint>, "LRendererID type mismatch");
+	};
+
+}
