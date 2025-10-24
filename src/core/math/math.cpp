f21db9f42155308d953310d0e84eb32d28b3a3ca
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-24 19:28:27 +0200

    math: add point converter functions

--- src/core/math/math.cpp
+++ src/core/math/math.cpp
@@ -2,4 +2,36 @@
 
 namespace platformer2d {
 
+	glm::vec3 ConvertScreenToWorld(const glm::vec3& Point, const glm::vec3& Center,
+								   const float Width, const float Height, const float Zoom)
+	{
+		const float Ratio = Width / Height;
+		const float U = Point.x / Width;
+		const float V = (Height - Point.y) / Height;
+
+		const glm::vec3 Extents = { Zoom * Ratio, Zoom, 0.0f };
+		const glm::vec3 Lower = Center - Extents;
+		const glm::vec3 Upper = Center + Extents;
+
+		return glm::vec3(
+			(1.0f - U) * Lower.x + U * Upper.x,
+			(1.0f - V) * Lower.y + V * Upper.y,
+			0.0f
+		);
+	}
+
+	glm::vec3 ConvertWorldToScreen(const glm::vec3& Point, const glm::vec3& Center,
+								   const float Width, const float Height, const float Zoom)
+	{
+		const float Ratio = Width / Height;
+		const glm::vec3 Extents = { Zoom * Ratio, Zoom, 0.0f };
+		const glm::vec3 Lower = Center - Extents;
+		const glm::vec3 Upper = Center + Extents;
+
+		const float U = (Point.x - Lower.x) / (Upper.x - Lower.x);
+		const float V = (Point.y - Lower.y) / (Upper.y - Lower.y);
+
+		return glm::vec3(U * Width, (1.0f - V) * Height, 0.0f);
+	}
+
 }
--- src/core/math/math.h
+++ src/core/math/math.h
@@ -6,4 +6,9 @@
 
 namespace platformer2d {
 
+	glm::vec3 ConvertScreenToWorld(const glm::vec3& Point, const glm::vec3& Center,
+								   float Width, float Height, float Zoom);
+	glm::vec3 ConvertWorldToScreen(const glm::vec3& Point, const glm::vec3& Center,
+								   float Width, float Height, float Zoom);
+
 }
