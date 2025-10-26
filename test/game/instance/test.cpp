84a5ae22a266b18c084057c4209900b26a05a983
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-26 22:03:01 +0100

    test: add game instance

---
+++ test/game/instance/test.cpp
@@ -0,0 +1,50 @@
+#include "test.h"
+
+#include <imgui/imgui.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <spdlog/spdlog.h>
+#include <stb/stb_image.h>
+
+#include "core/application.h"
+#include "core/assert.h"
+#include "core/window.h"
+#include "core/input/keyboard.h"
+#include "renderer/opengl.h"
+
+#include "game/level/testlevel.h"
+
+namespace platformer2d::test {
+
+	namespace
+	{
+		std::unique_ptr<CApplication> Application;
+		constexpr bool NO_TEST_INIT = false;
+	}
+
+	CTest::CTest(const int Argc, char* Argv[])
+		: CTestBase(Argc, Argv, NO_TEST_INIT)
+	{
+		Application = std::make_unique<CApplication>(Argc, Argv);
+	}
+
+	void CTest::Run()
+	{
+		Application->Initialize();
+
+		Running = true;
+		const int CatchResult = Catch::Session().run(Args.Argc, Args.Argv);
+
+		std::shared_ptr<Level::CTestLevel> TestLevel = std::make_shared<Level::CTestLevel>();
+		const bool LayerAdded = Application->PushLayer(TestLevel);
+		LK_VERIFY(LayerAdded, "Failed to add layer");
+
+		Application->Run();
+	}
+
+	void CTest::Destroy()
+	{
+		Application->Shutdown();
+	}
+
+}
