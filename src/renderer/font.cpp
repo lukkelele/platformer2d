385750ce1593f111708e481579f471137e71c953
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-10-31 21:20:45 +0100

    renderer: add font impl

---
+++ src/renderer/font.cpp
@@ -0,0 +1,73 @@
+#include "font.h"
+
+#include <array>
+
+namespace platformer2d::UI {
+
+	namespace
+	{
+		using TFontArray = std::array<ImFont*, static_cast<std::size_t>(EFontSize::COUNT)>;
+		using TFontMap = std::unordered_map<EFont, TFontArray>;
+
+		std::unordered_map<EFontModifier, TFontMap> FontMap = {
+			{ EFontModifier::Normal,     { { EFont::SourceSansPro, TFontArray{} } } },
+			{ EFontModifier::Bold  ,     { { EFont::Roboto,        TFontArray{} } } },
+			{ EFontModifier::Italic,     { { EFont::FontAwesome,   TFontArray{} } } },
+			{ EFontModifier::BoldItalic, { { EFont::FontAwesome,   TFontArray{} } } },
+			{ EFontModifier::SemiMedium, { { EFont::FontAwesome,   TFontArray{} } } },
+		};
+	}
+
+	const std::unordered_map<EFontSize, float> FontSizeMap = {
+		{ EFontSize::Regular, 22.0f },
+		{ EFontSize::Small,   12.0f },
+		{ EFontSize::Smaller, 16.0f },
+		{ EFontSize::Large,   26.0f },
+		{ EFontSize::Larger,  30.0f },
+		{ EFontSize::Header,  34.0f },
+		{ EFontSize::Title,   44.0f },
+	};
+
+	void Font::Add(const FFontConfiguration& FontConfig, const bool IsDefault)
+	{
+		LK_VERIFY((FontConfig.Font != EFont::None) && (FontConfig.Font != EFont::COUNT), "Invalid font");
+		LK_VERIFY((FontConfig.Size != EFontSize::None) && (FontConfig.Size != EFontSize::COUNT), "Invalid font size");
+		LK_VERIFY(std::filesystem::exists(FontConfig.FilePath), "Invalid font: {}", FontConfig.FilePath);
+
+		ImGuiIO& IO = ImGui::GetIO();
+		ImFontConfig ImguiFontConfig;
+		ImguiFontConfig.MergeMode = FontConfig.MergeWithLast;
+		ImFont* Font = IO.Fonts->AddFontFromFileTTF(
+			FontConfig.FilePath.string().c_str(),
+			FontSizeMap.at(FontConfig.Size),
+			&ImguiFontConfig,
+			(FontConfig.GlyphRanges == nullptr ? IO.Fonts->GetGlyphRangesDefault() : FontConfig.GlyphRanges)
+		);
+		LK_VERIFY(Font, "Failed to load font: {}", FontConfig.FilePath);
+
+		TFontArray& ArrayRef = FontMap.at(FontConfig.Modifier)[FontConfig.Font];
+		/* Verify the font hasn't been added yet. */
+		LK_VERIFY(ArrayRef[static_cast<int>(FontConfig.Size)] == nullptr, "Font {} ({}) already added", 
+				  Enum::ToString(FontConfig.Font), Enum::ToString(FontConfig.Size));
+		ArrayRef[static_cast<int>(FontConfig.Size)] = Font;
+		LK_DEBUG_TAG("Font", "Added: {} ({})", FontConfig.FilePath.filename(), FontSizeMap.at(FontConfig.Size));
+
+		if (IsDefault)
+		{
+			IO.FontDefault = Font;
+		}
+	}
+
+	void Font::Push(const EFont Font, const EFontSize Size, const EFontModifier Modifier)
+	{
+		ImFont* FontRef = FontMap.at(Modifier)[Font][static_cast<int>(Size)];
+		LK_ASSERT(FontRef, "Invalid font: {} ({}), {}", Enum::ToString(Font), Enum::ToString(Size), Enum::ToString(Modifier));
+		ImGui::PushFont(FontRef);
+	}
+
+	void Font::Pop()
+	{
+		ImGui::PopFont();
+	}
+
+}
---
+++ src/renderer/font.h
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
+
+#include "core/core.h"
+#include "fontawesome.h"
+
+namespace platformer2d {
+
+	enum class EFontSize
+	{
+		None,
+		Regular,
+		Smaller,
+		Small,
+		Large,
+		Larger,
+		Title,
+		Header,
+		COUNT
+	};
+
+	enum class EFontModifier
+	{
+		Normal,
+		Bold,
+		Italic,
+		BoldItalic,
+		SemiMedium,
+	};
+
+	enum class EFont
+	{
+		None,
+		SourceSansPro,
+		Roboto,
+		FontAwesome,
+		COUNT
+	};
+
+	struct FFontConfiguration
+	{
+		EFont Font;
+		EFontSize Size = EFontSize::Regular;
+		EFontModifier Modifier = EFontModifier::Normal;
+		std::filesystem::path FilePath{};
+		bool MergeWithLast = false;
+		const ImWchar* GlyphRanges = nullptr;
+	};
+
+	extern const std::unordered_map<EFontSize, float> FontSizeMap;
+
+	namespace UI::Font 
+	{
+		void Add(const FFontConfiguration& FontConfig, bool IsDefault = false);
+		void Push(EFont Font, EFontSize Size = EFontSize::Regular, EFontModifier Modifier = EFontModifier::Normal);
+		void Pop();
+	}
+
+	namespace Enum
+	{
+		static const char* ToString(const EFont Font)
+		{
+			switch (Font)
+			{
+				case EFont::None:          return "None";
+				case EFont::SourceSansPro: return "SourceSansPro";
+				case EFont::Roboto:        return "Roboto";
+				case EFont::FontAwesome:   return "FontAwesome";
+			}
+
+			LK_VERIFY(false);
+			return nullptr;
+		}
+
+		static const char* ToString(const EFontSize FontSize)
+		{
+			switch (FontSize)
+			{
+				case EFontSize::None:    return "None";
+				case EFontSize::Smaller: return "Smaller";
+				case EFontSize::Small:   return "Small";
+				case EFontSize::Regular: return "Regular";
+				case EFontSize::Large:   return "Large";
+				case EFontSize::Larger:  return "Larger";
+				case EFontSize::Header:  return "Header";
+				case EFontSize::Title:   return "Title";
+			}
+
+			LK_VERIFY(false);
+			return nullptr;
+		}
+
+		static const char* ToString(const EFontModifier FontModifier)
+		{
+			switch (FontModifier)
+			{
+				case EFontModifier::Normal:     return "Normal";
+				case EFontModifier::Bold:       return "Bold";
+				case EFontModifier::Italic:     return "Italic";
+				case EFontModifier::BoldItalic: return "BoldItalic";
+				case EFontModifier::SemiMedium: return "SemiMedium";
+			}
+
+			LK_VERIFY(false);
+			return nullptr;
+		}
+	}
+}
+
+namespace std 
+{
+    template<>
+    struct hash<::platformer2d::EFontModifier>
+    {
+        std::size_t operator()(const ::platformer2d::EFontModifier Modifier) const noexcept
+        {
+			return static_cast<std::size_t>(Modifier);
+        }
+    };
+
+    template<>
+    struct hash<::platformer2d::EFont>
+    {
+        std::size_t operator()(const ::platformer2d::EFont Font) const noexcept
+        {
+			return static_cast<std::size_t>(Font);
+        }
+    };
+
+}
