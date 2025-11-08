227d8de31c20fbbefbed8ce614ed8302ef55a66b
Lukas Gunnarsson <89297401+lukkelele@users.noreply.github.com>
2025-11-08 19:19:35 +0100

    player: character animation and movement states
    State machine for the different movement states

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -1,15 +1,61 @@
 #include "player.h"
 
-#include <imgui/imgui.h>
-
 #include "core/log.h"
-#include "core/input/keyboard.h"
 #include "core/window.h"
+#include "core/input/keyboard.h"
+#include "renderer/renderer.h"
 
 namespace platformer2d {
 
+	namespace
+	{
+		/** @todo: The Y tilepos does not start from index 0... */
+		constexpr int SPRITE_TILEPOS_Y = 2; /* Second row in the spritesheet. */
+
+		enum class ESpriteFrame : uint16_t
+		{
+			/* Cycle: 1->2->3->4 */
+			WalkStart = 1,
+			Walk2 = 2,
+			Walk3 = 3,
+			WalkEnd = 4,
+
+			JumpPreparation = 5,
+			JumpAscend = 6,
+			JumpDescend = 7,
+			JumpLanding = 8,
+
+			/* Cycle: 9->10->9 */
+			Hit1 = 9,
+			Hit2 = 10,
+
+			/* 11->12->13 (With preparation: 12->11->12->13) */
+			Slash1 = 11,
+			Slash2 = 12,
+			Slash3 = 13,
+
+			/* 14->12 (Variation: 12->14->12) */
+			Punch1 = 12,
+			Punch2 = 14,
+
+			COUNT
+		};
+
+		constexpr float VelocityThresholdX = CBody::LINEAR_VELOCITY_X_EPSILON;
+		constexpr float VelocityThresholdY = CBody::LINEAR_VELOCITY_Y_EPSILON;
+
+		constexpr std::array<EKey, 5> MovementKeys = {
+			EKey::W,
+			EKey::A,
+			EKey::S,
+			EKey::D,
+			EKey::Space
+		};
+	}
+
 	CPlayer::CPlayer(const FActorSpecification& Spec, const ETexture InTexture)
 		: CActor(Spec, InTexture)
+		, NextSpriteFrame(Enum::AsUnderlying(ESpriteFrame::COUNT))
 	{
 		Camera = std::make_unique<CCamera>(SCREEN_WIDTH, SCREEN_HEIGHT);
 		CWindow::OnResized.Add(this, &CPlayer::OnWindowResized);
@@ -20,11 +66,22 @@ namespace platformer2d {
 			Name = "Player";
 		}
 
-		LK_VERIFY(Body);
+		constexpr glm::vec2 TilePos = { ESpriteFrame::WalkStart, SPRITE_TILEPOS_Y };
+		WalkAnim.StartTileX = TilePos.x;
+		WalkAnim.StartTileY = TilePos.y;
+		WalkAnim.FrameCount = 4;
+		WalkAnim.TicksPerFrame = 18;
+		constexpr glm::vec2 TileSize = { 32, 32 };
+		LK_VERIFY(InTexture == ETexture::Player, "Player texture mismatch: {}", Enum::ToString(InTexture));
+		Sprite = std::make_unique<CSprite>(CRenderer::GetTexture(Texture), TilePos, TileSize);
+
+		Timer.Reset();
+		LK_VERIFY(Body && Sprite);
 	}
 
 	CPlayer::CPlayer(const FBodySpecification& BodySpec, const ETexture InTexture)
 		: CActor(BodySpec, InTexture)
+		, NextSpriteFrame(Enum::AsUnderlying(ESpriteFrame::COUNT))
 	{
 		Camera = std::make_unique<CCamera>(SCREEN_WIDTH, SCREEN_HEIGHT);
 		CWindow::OnResized.Add(this, &CPlayer::OnWindowResized);
@@ -35,28 +92,32 @@ namespace platformer2d {
 			Name = "Player";
 		}
 
-		LK_VERIFY(Body);
+		constexpr glm::vec2 TilePos = { ESpriteFrame::WalkStart, SPRITE_TILEPOS_Y };
+		WalkAnim.StartTileX = TilePos.x;
+		WalkAnim.StartTileY = TilePos.y;
+		WalkAnim.FrameCount = 4;
+		WalkAnim.TicksPerFrame = 24;
+		constexpr glm::vec2 TileSize = { 32, 32 };
+		LK_VERIFY(InTexture == ETexture::Player, "Player texture mismatch: {}", Enum::ToString(InTexture));
+		Sprite = std::make_unique<CSprite>(CRenderer::GetTexture(Texture), TilePos, TileSize);
+
+		Timer.Reset();
+		LK_VERIFY(Body && Sprite);
 	}
 
 	void CPlayer::Tick(const float DeltaTime)
 	{
 		CActor::Tick(DeltaTime);
 
-		SyncTransformComponent();
-		CheckJumpState();
+		CheckCollisions();
+		UpdateMovementState();
+		if (bShouldUpdateSprite)
+		{
+			UpdateSprite();
+		}
 
-		if (CKeyboard::IsKeyDown(EKey::A))
-		{
-			Body->ApplyForce({ -DirForce, 0.0f });
-		}
-		if (CKeyboard::IsKeyDown(EKey::D))
-		{
-			Body->ApplyForce({ DirForce, 0.0f });
-		}
-		if (CKeyboard::IsKeyDown(EKey::Space))
-		{
-			Jump();
-		}
+		HandleInput();
+		SyncTransformComponent();
 
 		if (bCameraLock)
 		{
@@ -70,6 +131,7 @@ namespace platformer2d {
 		if (!Data.bJumping)
 		{
 			Data.bJumping = true;
+			Data.MovementState = EMovementState::Airborne;
 			Body->ApplyImpulse({ 0.0f, JumpImpulse });
 			OnJumped.Broadcast(Data);
 		}
@@ -97,7 +159,138 @@ namespace platformer2d {
 		return Size;
 	}
 
-	void CPlayer::CheckJumpState()
+	void CPlayer::HandleInput()
+	{
+		if (CKeyboard::IsKeyDown(EKey::A))
+		{
+			Body->ApplyForce({ -DirForce, 0.0f });
+			LookDir = EDirection::Left;
+			LastDirForce = -DirForce;
+			bMovementInputLastTick = true;
+		}
+		if (CKeyboard::IsKeyDown(EKey::D))
+		{
+			Body->ApplyForce({ DirForce, 0.0f });
+			LookDir = EDirection::Right;
+			LastDirForce = DirForce;
+			bMovementInputLastTick = true;
+		}
+		if (CKeyboard::IsKeyDown(EKey::Space))
+		{
+			Jump();
+			LastDirForce = 0.0f;
+			bMovementInputLastTick = true;
+		}
+
+		/* Clear movement input flag if needed. */
+		if (bMovementInputLastTick && !CKeyboard::IsAnyKeysDown(MovementKeys))
+		{
+			LK_TRACE_TAG("Player", "Clear movement input tick flag");
+			LastDirForce = 0.0f;
+			bMovementInputLastTick = false;
+		}
+	}
+
+	void CPlayer::UpdateMovementState()
+	{
+		if (bJustLanded)
+		{
+			LK_TRACE_TAG("Player", "Just landed");
+			/* The idle state will get evaluated to idle/running later. */
+			SetMovementState(EMovementState::Idle);
+			bJustLanded = false;
+		}
+
+		switch (Data.MovementState)
+		{
+			case EMovementState::Idle:
+				MovementState_Idle();
+				break;
+
+			case EMovementState::Running:
+				MovementState_Running();
+				break;
+
+			case EMovementState::Airborne:
+				MovementState_Airborne();
+				break;
+		}
+
+		bShouldUpdateSprite = (CurrentSpriteFrame != NextSpriteFrame);
+	}
+
+	void CPlayer::MovementState_Idle()
+	{
+		const glm::vec2 LinearVelocity = Body->GetLinearVelocity();
+		const bool MovingByInput = (LastDirForce != 0.0f);
+
+		/* Movement in X-axis */
+		if (std::abs(LinearVelocity.x) > VelocityThresholdX)
+		{
+			if (MovingByInput)
+			{
+				SetMovementState(EMovementState::Running);
+				NextSpriteFrame = Enum::AsUnderlying(ESpriteFrame::WalkStart);
+			}
+			else
+			{
+				/* Player is moving because of external forces. */
+				NextSpriteFrame = Enum::AsUnderlying(ESpriteFrame::Hit1);
+			}
+		}
+		else
+		{
+			/* Player character is idle. */
+			NextSpriteFrame = Enum::AsUnderlying(ESpriteFrame::WalkStart);
+		}
+	}
+
+	void CPlayer::MovementState_Running()
+	{
+		const glm::vec2 LinearVelocity = Body->GetLinearVelocity();
+		const uint16_t FrameIndex = CRenderer::GetFrameIndex();
+		const bool MovingByInput = (LastDirForce != 0.0f);
+
+		if (std::abs(LinearVelocity.x) > VelocityThresholdX)
+		{
+			/* Increment the frame by 1 because the sprite sheet begins at position 1. */
+			const uint16_t AnimFrame = WalkAnim.CalculateAnimFrame(FrameIndex) + 1;
+			NextSpriteFrame = AnimFrame;
+		}
+		else if (!MovingByInput && std::abs(LinearVelocity.x) < VelocityThresholdX)
+		{
+			SetMovementState(EMovementState::Idle);
+			NextSpriteFrame = Enum::AsUnderlying(ESpriteFrame::WalkStart);
+		}
+	}
+
+	void CPlayer::MovementState_Airborne()
+	{
+		const glm::vec2 LinearVelocity = Body->GetLinearVelocity();
+
+		/* Check if ascending or descending. */
+		if (LinearVelocity.y > VelocityThresholdY)
+		{
+			NextSpriteFrame = Enum::AsUnderlying(ESpriteFrame::JumpAscend);
+		}
+		else if (LinearVelocity.y < -VelocityThresholdY)
+		{
+			NextSpriteFrame = Enum::AsUnderlying(ESpriteFrame::JumpDescend);
+		}
+	}
+
+	void CPlayer::SetMovementState(const EMovementState State)
+	{
+		if (Data.MovementState == State)
+		{
+			return;
+		}
+
+		LK_TRACE_TAG("Player", "{} -> {}", Enum::ToString(Data.MovementState), Enum::ToString(State));
+		Data.MovementState = State;
+	}
+
+	void CPlayer::CheckCollisions()
 	{
 		static constexpr int MAX_CONTACTS = 4;
 		const b2BodyId BodyID = Body->GetID();
@@ -120,27 +313,37 @@ namespace platformer2d {
 		if (bCanJump && Data.bJumping)
 		{
 			Data.bJumping = false;
+			bJustLanded = true;
 			OnLanded.Broadcast(Data);
 		}
 	}
 
 	void CPlayer::SyncTransformComponent()
 	{
-		if (Body)
+		const glm::vec2 BodySize = Body->GetSize();
+		TransformComp.Scale.x = BodySize.x;
+		TransformComp.Scale.y = BodySize.y;
+	}
+
+	void CPlayer::UpdateSprite()
+	{
+		SetSpriteTilePos(NextSpriteFrame);
+		LK_ASSERT(CurrentSpriteFrame == NextSpriteFrame);
+	}
+
+	void CPlayer::SetSpriteTilePos(const uint16_t X)
+	{
+		if (CurrentSpriteFrame != X)
 		{
-			const glm::vec2 BodySize = Body->GetSize();
-			TransformComp.Scale.x = BodySize.x;
-			TransformComp.Scale.y = BodySize.y;
+			const bool FlipHorizontal = (LookDir == EDirection::Left);
+			Sprite->SetTilePos(X, SPRITE_TILEPOS_Y, FlipHorizontal);
+			CurrentSpriteFrame = X;
 		}
 	}
 
-	/* @fixme This might be redundant as the camera does set the viewport size on every scene start */
 	void CPlayer::OnWindowResized(const uint16_t Width, const uint16_t Height)
 	{
-		if (Camera)
-		{
-			Camera->SetViewportSize(Width, Height);
-		}
+		LK_UNUSED(Width, Height);
 	}
 
 	void CPlayer::OnMouseScrolled(const EMouseScrollDirection Direction)
--- src/game/player.h
+++ src/game/player.h
@@ -1,18 +1,32 @@
 #pragma once
 
-#include <string>
-
-#include <glm/glm.hpp>
-
 #include "core/delegate.h"
+#include "core/timer.h"
 #include "renderer/camera.h"
+#include "renderer/sprite.h"
 #include "scene/actor.h"
 
 namespace platformer2d {
 
+	enum class EMovementState
+	{
+		Idle,
+		Running,
+		Airborne,
+	};
+
+	enum class EDirection
+	{
+		Up,
+		Down,
+		Left,
+		Right
+	};
+
 	struct FPlayerData
 	{
 		uint64_t ID = 0;
+		EMovementState MovementState = EMovementState::Idle;
 		bool bJumping = false;
 	};
 
@@ -31,12 +45,15 @@ namespace platformer2d {
 		virtual void Tick(float DeltaTime) override;
 		void Jump();
 
-		const FPlayerData& GetData() const { return Data; }
+		inline const FPlayerData& GetData() const { return Data; }
+		inline const CSprite& GetSprite() const { return *Sprite; }
 
-		inline float GetJumpImpulse() const { return JumpImpulse; }
+		float GetJumpImpulse() const { return JumpImpulse; }
 		void SetJumpImpulse(float Impulse);
-		inline float GetDirectionForce() const { return DirForce; }
+		float GetDirectionForce() const { return DirForce; }
 		void SetDirectionForce(float Force);
+		float GetLastDirectionForce() const { return LastDirForce; }
+		EDirection GetLookDirection() const { return LookDir; }
 
 		inline CCamera& GetCamera() { return *Camera; }
 		inline const CCamera& GetCamera() const { return *Camera; }
@@ -48,8 +65,18 @@ namespace platformer2d {
 		glm::vec2 GetSize() const;
 
 	private:
-		void CheckJumpState();
+		void HandleInput();
+
+		void UpdateMovementState();
+		void MovementState_Idle();
+		void MovementState_Running();
+		void MovementState_Airborne();
+		void SetMovementState(EMovementState State);
+
+		void CheckCollisions();
 		void SyncTransformComponent();
+		void UpdateSprite();
+		void SetSpriteTilePos(uint16_t X);
 
 		void OnWindowResized(uint16_t Width, uint16_t Height);
 		void OnMouseScrolled(EMouseScrollDirection Direction);
@@ -59,11 +86,61 @@ namespace platformer2d {
 		FOnLanded OnLanded;
 	private:
 		FPlayerData Data{};
+		CTimer Timer;
 		std::unique_ptr<CCamera> Camera = nullptr;
 		bool bCameraLock = true;
 
+		EDirection LookDir = EDirection::Right;
 		float JumpImpulse = 2.890f;
 		float DirForce = 5.630f;
+		float LastDirForce = 0.0f;
+
+		bool bJustLanded = false;
+		bool bMovementInputLastTick = false;
+		bool bShouldUpdateSprite = false;
+
+		std::unique_ptr<CSprite> Sprite = nullptr;
+		FSpriteAnimation WalkAnim;
+		uint16_t CurrentSpriteFrame = 0;
+		uint16_t NextSpriteFrame = 0;
 	};
+
+	namespace Enum
+	{
+		inline const char* ToString(const EMovementState State)
+		{
+			const char* S = "";
+		#define _(EnumValue) case EMovementState::EnumValue: S = #EnumValue; break
+			switch (State)
+			{
+				_(Idle);
+				_(Running);
+				_(Airborne);
+				default:
+					LK_THROW_ENUM_ERR(State);
+					break;
+			}
+		#undef _
+			return S;
+		}
+
+		inline const char* ToString(const EDirection Direction)
+		{
+			const char* S = "";
+		#define _(EnumValue) case EDirection::EnumValue: S = #EnumValue; break
+			switch (Direction)
+			{
+				_(Up);
+				_(Down);
+				_(Left);
+				_(Right);
+				default:
+					LK_THROW_ENUM_ERR(Direction);
+					break;
+			}
+		#undef _
+			return S;
+		}
+	}
 
 }
