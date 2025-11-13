37636ebf65ecdcaa52c7d362fa9439a21e19ec20
lukkelele <lukasgunnarsson@protonmail.com>
2025-11-13 11:52:01 +0100

    use correct GLFW include

--- test/test_base.cpp
+++ test/test_base.cpp
@@ -10,7 +10,7 @@
 #endif
 
 #include <glad/glad.h>
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 #include <imgui/imgui.h>
 #include <imgui/imgui_internal.h>
 #include <imgui/backends/imgui_impl_glfw.h>
