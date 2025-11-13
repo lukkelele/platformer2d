041574e92c5eaa0110963d1c0d9c01ff0f090842
lukkelele <lukasgunnarsson@protonmail.com>
2025-11-13 19:40:12 +0100

    core: use LK_FMT_LIB for formatters

--- src/core/log_formatters.h
+++ src/core/log_formatters.h
@@ -2,64 +2,71 @@
 
 #include <string>
 #include <filesystem>
+
+#ifdef LK_COMPILER_MSVC
 #include <format>
+#else
+#include <spdlog/fmt/fmt.h>
+#endif
 
 #include <glm/glm.hpp>
 
+#include "macros.h"
+
 template<>
-struct std::formatter<std::filesystem::path> : std::formatter<std::string>
+struct LK_FMT_LIB::formatter<std::filesystem::path> : LK_FMT_LIB::formatter<std::string>
 {
-	template<typename FormatContext>
+    template<typename FormatContext>
     auto format(const std::filesystem::path& Input, FormatContext& Context) const
     {
-        return std::format_to(Context.out(), "{}", Input.generic_string());
+        return LK_FMT_LIB::format_to(Context.out(), "{}", Input.generic_string());
     }
 };
 
 template<>
-struct std::formatter<glm::vec2>
+struct LK_FMT_LIB::formatter<glm::vec2>
 {
-	template<typename ParseContext>
+    template<typename ParseContext>
     constexpr auto parse(ParseContext& Context)
     {
         return Context.begin();
     }
 	
-	template<typename FormatContext>
+    template<typename FormatContext>
     auto format(const glm::vec2& Input, FormatContext& Context) const
     {
-        return std::format_to(Context.out(), "({:.2f}, {:.2f})", Input.x, Input.y);
+        return LK_FMT_LIB::format_to(Context.out(), "({:.2f}, {:.2f})", Input.x, Input.y);
     }
 };
 
 template<>
-struct std::formatter<glm::vec3>
+struct LK_FMT_LIB::formatter<glm::vec3>
 {
-	template<typename ParseContext>
+    template<typename ParseContext>
     constexpr auto parse(ParseContext& Context)
     {
         return Context.begin();
     }
-	
-	template<typename FormatContext>
+
+    template<typename FormatContext>
     auto format(const glm::vec3& Input, FormatContext& Context) const
     {
-        return std::format_to(Context.out(), "({:.2f}, {:.2f}, {:.2f})", Input.x, Input.y, Input.z);
+        return LK_FMT_LIB::format_to(Context.out(), "({:.2f}, {:.2f}, {:.2f})", Input.x, Input.y, Input.z);
     }
 };
 
 template<>
-struct std::formatter<glm::vec4>
+struct LK_FMT_LIB::formatter<glm::vec4>
 {
-	template<typename ParseContext>
+    template<typename ParseContext>
     constexpr auto parse(ParseContext& Context)
     {
         return Context.begin();
     }
-	
-	template<typename FormatContext>
+
+    template<typename FormatContext>
     auto format(const glm::vec4& Input, FormatContext& Context) const
     {
-        return std::format_to(Context.out(), "({:.2f}, {:.2f}, {:.2f}, {:.2f})", Input.x, Input.y, Input.z, Input.z);
+        return LK_FMT_LIB::format_to(Context.out(), "({:.2f}, {:.2f}, {:.2f}, {:.2f})", Input.x, Input.y, Input.z, Input.z);
     }
 };
