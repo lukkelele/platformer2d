42093b7c50be0b0a5395a73be8e29ea073768896
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-10 21:45:10 +0100

    renderer: texture: add rifle to enum

--- src/renderer/texture.h
+++ src/renderer/texture.h
@@ -20,6 +20,7 @@ namespace platformer2d {
 		Wood,
 		Swoosh,
 		Cloud,
+		Rifle,
 		COUNT
 	};
 
@@ -86,6 +87,7 @@ namespace platformer2d {
 				case ETexture::Wood:       return "Wood";
 				case ETexture::Swoosh:     return "Swoosh";
 				case ETexture::Cloud:      return "Cloud";
+				case ETexture::Rifle:      return "Rifle";
 				case ETexture::COUNT:      return "COUNT";
 				default: break;
 			}
