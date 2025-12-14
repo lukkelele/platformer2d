5a5d9cfd1da1d1dd78f78d87edb752b39ecbde1f
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-14 20:50:09 +0100

    game: inventory: implement base logic
    Base implementation for adding and removing items.
    In the future the inventory should take ownership of all added items.
    
    Currently the player weapons are referenced by the player instance
    itself, this will change in the near future.

--- src/game/inventory.cpp
+++ src/game/inventory.cpp
@@ -1,1 +1,101 @@
 #include "inventory.h"
+
+namespace platformer2d {
+
+	CInventory::CInventory(std::string_view InName)
+		: Name(InName)
+	{
+		LK_DEBUG_TAG("Inventory", "Created: {}", Name);
+	}
+
+	CInventory::~CInventory()
+	{
+		LK_ASSERT(!IsFull() && (GetFreeSlots() == Items.size()));
+	}
+
+	void CInventory::Destroy()
+	{
+		LK_DEBUG_TAG("Inventory", "Release: {} (Occupied {}/{})", Name, (Items.size() - GetFreeSlots()), Items.size());
+
+		for (FInventoryItem& Entry : Items) {
+			/* @todo: Serialize every entry */
+			if (!Entry.bValid) {
+				continue;
+			}
+
+			LK_DEBUG_TAG("Inventory", "Release: {}", Enum::ToString(Entry.ItemRef->GetItemType()));
+			Entry.ItemRef.reset();
+		}
+
+		LK_ASSERT(!IsFull() && (GetFreeSlots() == Items.size()));
+	}
+
+	bool CInventory::IsFull() const
+	{
+		for (std::size_t Idx = 0; Idx < Items.size(); Idx++) {
+			if (Items.at(Idx).ItemRef == nullptr) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	std::size_t CInventory::GetFreeSlots() const
+	{
+		std::size_t Free = 0;
+		for (std::size_t Idx = 0; Idx < Items.size(); Idx++) {
+			if (Items.at(Idx).ItemRef == nullptr) {
+				Free++;
+			}
+		}
+
+		return Free;
+	}
+
+	bool CInventory::GetNextFreeIdx(std::size_t& Idx) const
+	{
+		Idx = 0;
+		for (Idx; Idx < Items.size(); Idx++) {
+			if (Items.at(Idx).ItemRef == nullptr) {
+				return true;
+			}
+		}
+
+		Idx = std::numeric_limits<std::size_t>::max();
+		return false;
+	}
+
+	bool CInventory::Serialize(YAML::Emitter& Out, const EExtendableSerializer Extendable) const
+	{
+		LK_UNUSED(Extendable);
+		LK_DEBUG_TAG("Inventory", "Serializing: {}", Name);
+
+		Out << YAML::Key << "Inventory";
+		Out << YAML::BeginMap;
+		Out << YAML::Key << "PlayerIdx" << YAML::Value << PlayerIdx;
+
+		Out << YAML::Key << "Items";
+		Out << YAML::Value << YAML::BeginSeq;
+		for (std::size_t Idx = 0; Idx < Items.size(); Idx++) {
+			Out << YAML::BeginMap;
+			Out << YAML::Key << "PlayerIdx" << YAML::Value << PlayerIdx;
+			Out << YAML::EndMap;
+		}
+		Out << YAML::Value << YAML::EndSeq;
+
+		Out << YAML::EndMap; /* ~Inventory */
+
+		return true;
+	}
+
+	void CInventory::ClearSlot(const std::size_t Idx)
+	{
+		LK_ASSERT(Idx < Items.size());
+		FInventoryItem& Entry = Items.at(Idx);
+		Entry.ItemRef.reset();
+		Entry.Type = EItemType::None;
+		Entry.bValid = false;
+	}
+
+}
--- src/game/inventory.h
+++ src/game/inventory.h
@@ -1,7 +1,137 @@
 #pragma once
 
 #include "core/core.h"
+#include "serialization/serializable.h"
+#include "item.h"
 
 namespace platformer2d {
+
+	/* @todo: Move to its own file */
+	class CConsumable : public IItem
+	{
+	public:
+		CConsumable() = default;
+		~CConsumable() = default;
+
+		virtual EItemType GetItemType() const override { return EItemType::Consumable; }
+	};
+
+	class CInventory : public ISerializable<ESerializable::Yaml>
+	{
+	private:
+		struct FInventoryItem
+		{
+			EItemType Type = EItemType::None;
+			std::shared_ptr<IItem> ItemRef = nullptr;
+			bool bValid = false;
+		};
+	public:
+		CInventory(std::string_view InName);
+		CInventory() = delete;
+		~CInventory();
+
+		void Destroy();
+
+		template<typename T>
+		bool AddItem(std::shared_ptr<T> Item)
+		{
+			static_assert(std::is_base_of_v<IItem, T>, "T must implement the IItem interface");
+			if (!Item) {
+				return false;
+			}
+
+			std::size_t FreeIdx = 0;
+			if (!GetNextFreeIdx(FreeIdx)) {
+				return false;
+			}
+
+			LK_INFO_TAG("Inventory", "[{}] Add {} to slot {}", Name, Enum::ToString(Item->GetItemType()), FreeIdx);
+			FInventoryItem& Entry = Items.at(FreeIdx);
+			Entry.ItemRef = Item;
+			Entry.Type = Item->GetItemType();
+			Entry.bValid = true;
+
+			return true;
+		}
+
+		template<typename T>
+		std::shared_ptr<T> GetItem(const std::size_t Idx)
+		{
+			static_assert(std::is_base_of_v<IItem, T>, "T must implement the IItem interface");
+			LK_ASSERT(Idx < Items.size());
+			const std::shared_ptr<IItem>& Ref = Items.at(Idx).ItemRef;
+			if (!Ref) {
+				return nullptr;
+			}
+
+			return std::static_pointer_cast<T>(Ref);
+		}
+
+		template<typename T>
+		bool RemoveItem(const std::size_t Idx)
+		{
+			static_assert(std::is_base_of_v<IItem, T>, "T must implement the IItem interface");
+			LK_ASSERT(Idx < Items.size());
+			const FInventoryItem& Entry = Items.at(Idx);
+			if (!Entry.bValid || !Entry.ItemRef) {
+				return false;
+			}
+
+			LK_INFO_TAG("Inventory", "[{}] Remove {} from slot {}", Name, Enum::ToString(Entry.Type), Idx);
+#ifdef LK_INVENTORY_SANITY_CAST
+			std::static_pointer_cast<T>(Entry.ItemRef);
+#endif
+			ClearSlot(Idx);
+			return true;
+		}
+
+		template<typename T>
+		bool RemoveItem(const std::shared_ptr<T>& Item)
+		{
+			static_assert(std::is_base_of_v<IItem, T>, "T must implement the IItem interface");
+			if (!Item) {
+				return false;
+			}
+
+			for (std::size_t Idx = 0; Idx < Items.size(); ++Idx) {
+				const FInventoryItem& Entry = Items.at(Idx);
+				if (!Entry.bValid) {
+					continue;
+				}
+
+				if (Entry.ItemRef == Item) {
+					LK_INFO_TAG("Inventory", "[{}] Remove {} from slot {}", Name, Enum::ToString(Entry.Type), Idx);
+					ClearSlot(Idx);
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		bool IsFull() const;
+		std::size_t GetFreeSlots() const;
+		bool GetNextFreeIdx(std::size_t& Idx) const;
+
+		uint8_t GetAssignedPlayerIndex() const { return PlayerIdx; }
+		std::string_view GetName() const { return Name; }
+
+		virtual bool Serialize(YAML::Emitter& Out, EExtendableSerializer Extendable = EExtendableSerializer::No) const override;
+
+	private:
+		void ClearSlot(std::size_t Idx);
+
+		CInventory(const CInventory&) = delete;
+		CInventory(CInventory&&) = delete;
+		CInventory& operator=(const CInventory&) = delete;
+		CInventory& operator=(CInventory&&) = delete;
+
+	public:
+		static constexpr std::size_t MAX_ITEMS = 6;
+	private:
+		uint8_t PlayerIdx = 0;
+		std::string Name;
+		std::array<FInventoryItem, MAX_ITEMS> Items;
+	};
 
 }
