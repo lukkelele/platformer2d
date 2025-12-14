c6319849d0409fafa5877e3504f50ff5e5ae1df8
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-14 20:46:54 +0100

    game: add item interface

---
+++ src/game/item.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "core/core.h"
+
+namespace platformer2d {
+
+	enum class EItemType
+	{
+		None,
+		Consumable,
+		Weapon,
+		COUNT
+	};
+
+	class IItem
+	{
+	public:
+		virtual ~IItem() = default;
+		virtual EItemType GetItemType() const = 0;
+	};
+
+	namespace Enum {
+		inline const char* ToString(const EItemType Item)
+		{
+			const char* S = "";
+		#define _(EnumValue) case EItemType::EnumValue: S = #EnumValue; break
+			switch (Item) {
+				_(None);
+				_(Consumable);
+				_(Weapon);
+				_(COUNT);
+				default:
+					LK_THROW_ENUM_ERR(Item);
+					break;
+			}
+		#undef _
+			return S;
+		}
+	}
+
+}
