adc504254a939f7e2bc999715e4db925c8e74841
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-16 19:43:35 +0100

    game: instance: add pause/resume to interface

--- src/game/instance.h
+++ src/game/instance.h
@@ -47,6 +47,9 @@ namespace platformer2d {
 		virtual std::shared_ptr<CScene> GetScene() const = 0;
 		bool HasScene() const { return GetScene() != nullptr; }
 
+		virtual void PauseGame() = 0;
+		virtual void ResumeGame() = 0;
+
 		virtual uint16_t RaycastScene(std::shared_ptr<CScene> TargetScene, std::vector<FHitResult>& HitResults) = 0;
 		virtual uint16_t PickSceneAtMouse(std::shared_ptr<CScene> TargetScene, std::vector<FHitResult>& HitResults) = 0;
 
