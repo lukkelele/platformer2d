9a16b795b35d0bd63a8e19c18803b7368e01aa90
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-21 19:55:25 +0100

    ui: combo template for C arrays

--- src/renderer/ui/combo.h
+++ src/renderer/ui/combo.h
@@ -37,6 +37,36 @@ namespace platformer2d::UI {
 		return Updated;
 	}
 
+	template<typename TEnum>
+	inline bool Combo(std::string_view Label, const char*& Options, const std::size_t N, TEnum& Selected)
+	{
+		char NameBuf[64] = { 0 };
+		std::snprintf(NameBuf, sizeof(NameBuf), "%s", Label.data());
+		if (!ImGui::BeginCombo(NameBuf, Enum::ToString(Selected))) {
+			return false;
+		}
+
+		bool Updated = false;
+		for (std::size_t Idx = 0; Idx < N; Idx++) {
+			const char* Option = Options[Idx];
+			if (Option == nullptr) {
+				continue;
+			}
+
+			const bool IsSelected = (std::to_underlying(Selected) == Idx);
+			if (ImGui::Selectable(Option, IsSelected)) {
+				if (std::to_underlying(Selected) != Idx) {
+					Updated = true;
+					Selected = static_cast<TEnum>(Idx);
+				}
+			}
+		}
+
+		ImGui::EndCombo();
+		return Updated;
+	}
+
+
 	namespace Widget::Combo {
 		void TextureModifier();
 		bool TextureDropdown(ETexture& Selected);
