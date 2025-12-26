47b1a9ba38aac85b70306124004e60eb42da856e
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-26 20:43:26 +0100

    game: controller: patrol controller targeting

--- src/game/controller/patrolcontroller.cpp
+++ src/game/controller/patrolcontroller.cpp
@@ -1,6 +1,7 @@
 #include "patrolcontroller.h"
 
 #include "game/enemy.h"
+#include "game/instance.h"
 
 namespace platformer2d {
 
@@ -13,7 +14,7 @@ namespace platformer2d {
 
 	void CPatrolController::OnPossess(CEnemy& Enemy)
 	{
-		LK_WARN_TAG("PatrolController", "OnPossess -> {}", Enemy.GetName());
+		LK_DEBUG_TAG("PatrolController", "OnPossess -> {}", Enemy.GetName());
 		StartX = Enemy.GetPosition().x;
 		Enemy.GetData().State = EEnemyState::Patrolling;
 		PatrolDirection = Enemy.GetData().LookDirection;
@@ -33,27 +34,109 @@ namespace platformer2d {
 			return;
 		}
 
-		const float X = Enemy.GetPosition().x;
-		const float MinX = StartX - PatrolHalfDistance;
-		const float MaxX = StartX + PatrolHalfDistance;
+		if (std::shared_ptr<CActor> Target = TargetRef.lock(); Target != nullptr) {
+			const ETargetResponse Response = HandleTarget(Enemy.GetPosition(), Target);
+			switch (Response) {
+				case ETargetResponse::None:
+					break;
+				case ETargetResponse::MoveLeft:
+					Enemy.MoveLeft();
+					break;
+				case ETargetResponse::MoveRight:
+					Enemy.MoveRight();
+					break;
+				case ETargetResponse::StopMovement:
+					Enemy.StopMovement();
+					break;
+			}
+		} else {
+			const float X = Enemy.GetPosition().x;
+			const float MinX = StartX - PatrolHalfDistance;
+			const float MaxX = StartX + PatrolHalfDistance;
 
-		if ((PatrolDirection == EDirection::Right) && (X >= MaxX)) {
-			PatrolDirection = EDirection::Left;
-		}
-		if ((PatrolDirection == EDirection::Left) && (X <= MinX)) {
-			PatrolDirection = EDirection::Right;
-		}
+			if ((PatrolDirection == EDirection::Right) && (X >= MaxX)) {
+				PatrolDirection = EDirection::Left;
+			}
+			if ((PatrolDirection == EDirection::Left) && (X <= MinX)) {
+				PatrolDirection = EDirection::Right;
+			}
 
-		if (PatrolDirection == EDirection::Left) {
-			Enemy.MoveLeft();
-		} else if (PatrolDirection == EDirection::Right) {
-			Enemy.MoveRight();
+			if (PatrolDirection == EDirection::Left) {
+				Enemy.MoveLeft();
+			} else if (PatrolDirection == EDirection::Right) {
+				Enemy.MoveRight();
+			}
 		}
+	}
+
+	void CPatrolController::SetTarget(std::shared_ptr<CActor> InTarget)
+	{
+		LK_DEBUG_TAG("PatrolController", "Targeting: {}", (InTarget ? InTarget->GetName() : "NULL"));
+		TargetRef = InTarget;
 	}
 
 	void CPatrolController::SetPatrolDirection(const EDirection InDirection)
 	{
 		PatrolDirection = InDirection;
+	}
+
+	void CPatrolController::SetDetectRadius(const float InRadius)
+	{
+		DetectRadius = InRadius;
+	}
+
+	void CPatrolController::SetStopRadius(const float InRadius)
+	{
+		StopRadius = InRadius;
+	}
+
+	CPatrolController::ETargetResponse CPatrolController::HandleTarget(const glm::vec3& CurrentPos, const std::shared_ptr<CActor>& Target)
+	{
+		if (!Target) {
+			return ETargetResponse::None;
+		}
+
+		const glm::vec2 Pos = glm::vec2(CurrentPos.x, CurrentPos.y);
+		const glm::vec3 TargetPos3 = Target->GetPosition();
+		const glm::vec2 TargetPos = glm::vec2(TargetPos3.x, TargetPos3.y);
+
+		const glm::vec2 Delta = TargetPos - Pos;
+		const float DistSq = glm::dot(Delta, Delta);
+
+		const float DetectRadiusSq = DetectRadius * DetectRadius;
+		const float StopRadiusSq = StopRadius * StopRadius;
+		const float GiveUpRadiusSq = GiveUpRadius * GiveUpRadius;
+
+		if (!bChasing) {
+			if (DistSq <= DetectRadiusSq) {
+				bChasing = true;
+			}
+		} else {
+			if (DistSq > GiveUpRadiusSq) {
+				bChasing = false;
+			}
+		}
+
+		if (bChasing) {
+			if (DistSq <= StopRadiusSq) {
+				return ETargetResponse::StopMovement;
+			}
+
+			static constexpr float THRESHOLD_X = 0.10f;
+			const float DeltaX = TargetPos.x - CurrentPos.x;
+
+			if (std::abs(DeltaX) <= THRESHOLD_X) {
+				return ETargetResponse::StopMovement;
+			}
+
+			if (DeltaX < 0.0f) {
+				return ETargetResponse::MoveLeft;
+			} else {
+				return ETargetResponse::MoveRight;
+			}
+		}
+
+		return ETargetResponse::None;
 	}
 
 }
--- src/game/controller/patrolcontroller.h
+++ src/game/controller/patrolcontroller.h
@@ -4,6 +4,8 @@
 
 namespace platformer2d {
 
+	class CActor;
+
 	class CPatrolController : public IEnemyController
 	{
 	public:
@@ -15,20 +17,43 @@ namespace platformer2d {
 
 		virtual EControllerType GetControllerType() const override { return EControllerType::Patrol; }
 
+		void SetTarget(std::shared_ptr<CActor> InTarget);
+		bool HasTarget() const { return !TargetRef.expired(); }
+
 		void SetPatrolDirection(EDirection InDirection);
 		EDirection GetPatrolDirection() const { return PatrolDirection; }
 
+		bool IsChasing() const { return bChasing; }
 		float GetHalfDistance() const { return PatrolHalfDistance; }
 		float GetStartDelayInSeconds() const { return StartDelaySeconds; }
+		void SetDetectRadius(float InRadius);
+		float GetDetectRadius() const { return DetectRadius; }
+		void SetStopRadius(float InRadius);
+		float GetStopRadius() const { return StopRadius; }
+
+	private:
+		enum class ETargetResponse
+		{
+			None,
+			StopMovement,
+			MoveLeft,
+			MoveRight
+		};
+		ETargetResponse HandleTarget(const glm::vec3& CurrentPos, const std::shared_ptr<CActor>& Target);
 
 	private:
 		float StartX = 0.0f;
 		float PatrolHalfDistance = 2.0f;
+		std::weak_ptr<CActor> TargetRef;
+		EDirection PatrolDirection = EDirection::Right;
 
 		float StartDelaySeconds = 0.0f;
 		float ElapsedSeconds = 0.0f;
 
-		EDirection PatrolDirection = EDirection::Right;
+		bool bChasing = false;
+		float DetectRadius = 4.0f;
+		float StopRadius = 0.60f;
+		float GiveUpRadius = 6.0f;
 	};
 
 }
