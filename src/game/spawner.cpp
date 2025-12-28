5d3dee9098b299f2de676bc0db2eb4ddda6c6fd9
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2025-12-28 18:44:13 +0100

    game: spawner: randomized polygon name

--- src/game/spawner.cpp
+++ src/game/spawner.cpp
@@ -37,13 +37,16 @@ namespace platformer2d {
 	std::shared_ptr<CActor> CSpawner::CreatePolygon(std::string_view Name, const FBodySpecification& InBodySpec,
 													const glm::vec2& Size, const glm::vec4& Color, const ETexture Texture)
 	{
-		LK_VERIFY(!Name.empty(), "Name cannot be empty");
+		std::string ActorName(Name);
+		if (ActorName.empty()) {
+			ActorName = LK_FMT("Polygon{}", static_cast<uint16_t>(Math::Randomize(0, std::numeric_limits<uint16_t>::max())));
+		}
 		CGameInstance* GameInstance = CGameInstance::Get();
 		LK_VERIFY(GameInstance);
 		std::shared_ptr<CScene> Scene = GameInstance->GetScene();
 
 		FActorSpecification ActorSpec;
-		ActorSpec.Name = Name;
+		ActorSpec.Name = ActorName;
 		ActorSpec.Texture = Texture;
 		ActorSpec.Color = Color;
 
