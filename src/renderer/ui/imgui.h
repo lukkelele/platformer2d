7c10ffa48fc4601587a101309032ba320fcc93c1
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2026-05-03 15:21:33 +0200

    ui: format file

--- src/renderer/ui/imgui.h
+++ src/renderer/ui/imgui.h
@@ -7,16 +7,13 @@
 #include <imgui/imgui_internal.h>
 
 namespace ImGui {
-
-	inline bool TreeNodeBehaviorIsOpen(const ImGuiID NodeID,
-		const ImGuiTreeNodeFlags Flags = ImGuiTreeNodeFlags_None)
+	inline bool TreeNodeBehaviorIsOpen(const ImGuiID NodeID, const ImGuiTreeNodeFlags Flags = ImGuiTreeNodeFlags_None)
 	{
 		ImGuiContext& G = *GImGui;
 		ImGuiWindow* Window = G.CurrentWindow;
 		const bool IsOpen = Window->StateStorage.GetInt(NodeID, (Flags & ImGuiTreeNodeFlags_DefaultOpen) ? 1 : 0) != 0;
 		return IsOpen;
 	}
-
 }
 
 template<>
