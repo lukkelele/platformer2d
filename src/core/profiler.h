981730d8f4b6b532f1af11ff4b65493f09d4f308
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2026-05-09 18:51:00 +0200

    core: profiler: fix macros

--- src/core/profiler.h
+++ src/core/profiler.h
@@ -11,7 +11,7 @@
 #		define LK_PROFILE_SCOPE(Name) CScopedTimer LK_CONCAT_EXPAND(__LK_SCOPE_TIMER_, __LINE__)(Name);
 #	else
 #		define LK_PROFILE_SCOPE(Name) /* @todo: Tracy impl of some sort */
-#	endif                             /* LK_PROFILING_USE_SCOPED_TIMERS */
+#	endif /* LK_PROFILING_USE_SCOPED_TIMERS */
 
 #	define LK_PROFILE_MARK_FRAME()           FrameMark
 #	define LK_PROFILE_MARK_FRAME_BEGIN(Name) FrameMarkStart(Name)
@@ -21,7 +21,10 @@
 
 #else /* PROFILING DISABLED */
 #	define LK_PROFILE_SCOPE(Name)
-#	define LK_PROFILE_MARK_FRAME
+#	define LK_PROFILE_MARK_FRAME()
+#	define LK_PROFILE_MARK_FRAME_BEGIN(Name)
+#	define LK_PROFILE_MARK_FRAME_END(Name)
 #	define LK_PROFILE_FUNC(...)
 #	define LK_PROFILE_THREAD(...)
 #endif /* LK_PROFILING */
+
