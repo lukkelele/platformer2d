15b25757f1f23398ae70c2f184d6f6dfb8cd7bca
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2026-05-09 13:47:04 +0200

    core: buffer: format file

--- src/core/buffer.h
+++ src/core/buffer.h
@@ -7,7 +7,7 @@ namespace platformer2d {
 
 	struct FBuffer
 	{
-		explicit FBuffer(const void* InData, const uint64_t InSize)
+		explicit FBuffer(const void* InData, const std::uint64_t InSize)
 			: Data((void*)InData)
 			, Size(InSize)
 		{
@@ -68,7 +68,7 @@ namespace platformer2d {
 		FBuffer& operator=(FBuffer&& Other) noexcept
 		{
 			if (this != &Other) {
-				delete[] (uint8_t*)Data;
+				delete[] (std::uint8_t*)Data;
 
 				/* Transfer ownership. */
 				Allocate(Other.Size);
@@ -92,22 +92,19 @@ namespace platformer2d {
 			return Buffer;
 		}
 
-		static FBuffer Copy(const void* Data, const uint64_t Size)
+		static FBuffer Copy(const void* Data, const std::uint64_t Size)
 		{
 			FBuffer Buffer;
 			Buffer.Allocate(Size);
 			std::memcpy(Buffer.Data, Data, Size);
-
 			return Buffer;
 		}
 
-		void Allocate(const uint64_t InSize)
+		void Allocate(const std::uint64_t InSize)
 		{
-			LK_ASSERT(InSize > 0, "Allocate failed, invalid size: {}", InSize);
-			delete[] (uint8_t*)Data;
-			Data = nullptr;
-
-			Data = new uint8_t[InSize];
+			LK_ASSERT(InSize > 0, "Invalid size: {}", InSize);
+			delete[] (std::uint8_t*)Data;
+			Data = new std::uint8_t[InSize];
 			Size = InSize;
 		}
 
@@ -115,42 +112,35 @@ namespace platformer2d {
 		{
 			if (Data) {
 				LK_TRACE_TAG("Buffer", "Release {} bytes: {}", Size, Data);
-				delete[] (uint8_t*)Data;
+				delete[] (std::uint8_t*)Data;
 				Data = nullptr;
 				Size = 0;
 			}
 		}
 
 		template<typename T>
-		T& Read(const uint64_t Offset = 0)
+		T& Read(const std::uint64_t Offset = 0)
 		{
-			return *(T*)(static_cast<uint8_t*>(Data) + Offset);
+			return *(T*)(static_cast<std::uint8_t*>(Data) + Offset);
 		}
 
 		template<typename T>
-		const T& Read(uint64_t Offset = 0) const
+		const T& Read(std::uint64_t Offset = 0) const
 		{
-			return *(T*)(static_cast<uint8_t*>(Data) + Offset);
+			return *(T*)(static_cast<std::uint8_t*>(Data) + Offset);
 		}
 
-		void Write(const void* InData, const uint64_t WriteSize, uint64_t Offset = 0)
+		void Write(const void* InData, const std::uint64_t WriteSize, std::uint64_t Offset = 0) const
 		{
-			LK_ASSERT(Offset + WriteSize <= Size, "FBuffer overflow");
-			std::memcpy((uint8_t*)Data + Offset, InData, WriteSize);
+			LK_ASSERT(Offset + WriteSize <= Size, "Buffer overflow, WriteSize={} Offset={}", WriteSize, Offset);
+			std::memcpy((std::uint8_t*)Data + Offset, InData, WriteSize);
 		}
 
 		operator bool() { return (Data && (Size > 0)); }
 		operator bool() const { return (Data && (Size > 0)); }
 
-		uint8_t& operator[](const int Idx)
-		{
-			return ((uint8_t*)Data)[Idx];
-		}
-
-		uint8_t operator[](const int Idx) const
-		{
-			return ((uint8_t*)Data)[Idx];
-		}
+		std::uint8_t& operator[](const int Idx) { return ((std::uint8_t*)Data)[Idx]; }
+		std::uint8_t operator[](const int Idx) const { return ((std::uint8_t*)Data)[Idx]; }
 
 		template<typename T>
 		T* As() const
@@ -158,11 +148,11 @@ namespace platformer2d {
 			return (T*)Data;
 		}
 
-		uint64_t GetSize() const { return Size; }
+		std::uint64_t GetSize() const { return Size; }
 
 	public:
 		void* Data = nullptr;
-		uint64_t Size = 0;
+		std::uint64_t Size = 0;
 	};
 
 }
