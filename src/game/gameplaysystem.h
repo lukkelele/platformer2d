f233e1232efbeb8d8d89fed48f837c22087ccce9
Lukas Gunnarsson <lukasgunnarsson@protonmail.com>
2026-05-13 07:52:56 +0200

    game: gameplaysystem: implement interface

--- src/game/gameplaysystem.h
+++ src/game/gameplaysystem.h
@@ -1,22 +1,30 @@
 #pragma once
 
 #include "core/core.h"
+#include "gamesystem.h"
 
 namespace platformer2d {
 
 	class CActor;
 
-	class CGameplaySystem
+	/* @todo: Rename this class to something else */
+	class CGameplaySystem : public IGameSystem
 	{
 	public:
-		CGameplaySystem() = delete;
-		~CGameplaySystem() = delete;
+		CGameplaySystem() = default;
 		CGameplaySystem(CGameplaySystem&&) = delete;
 		CGameplaySystem(const CGameplaySystem&) = delete;
+		~CGameplaySystem() = default;
 
-		static bool Teleport(CActor* Source, const glm::vec2& Destination);
-		static bool Teleport(std::shared_ptr<CActor> Source, const glm::vec2& Destination);
-		static bool Teleport(std::shared_ptr<CActor> Source, std::shared_ptr<CActor> Destination);
+		CGameplaySystem& operator=(CGameplaySystem&&) = delete;
+		CGameplaySystem& operator=(const CGameplaySystem&) = delete;
+
+		void Initialize(CGameInstance& Owner) override {}
+		void Shutdown() override {}
+
+		bool Teleport(CActor* Source, const glm::vec2& Destination);
+		bool Teleport(std::shared_ptr<CActor> Source, const glm::vec2& Destination);
+		bool Teleport(std::shared_ptr<CActor> Source, std::shared_ptr<CActor> Destination);
 	};
 
 }
